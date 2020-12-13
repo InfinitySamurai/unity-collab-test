978dcc555c94b3e8e7689f0438346f4e1c94efe6
Russell Long <russelllongj@gmail.com>
2020-12-13 22:00:35 +1100

    Create a sphere manager to spawn spheres

---
+++ CollaborationTest/Assets/Scripts/SphereManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereManager : MonoBehaviour
+{
+    public GameObject spherePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(spherePrefab, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
