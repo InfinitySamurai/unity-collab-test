1fb6d12143aa9ab2510e1a8235283a5efc5124aa
Russell Long <russelllongj@gmail.com>
2020-12-18 20:04:51 +1100

    Make the spawner spawn things over time to a max

--- CollaborationTest/Assets/Scripts/SphereManager.cs
+++ CollaborationTest/Assets/Scripts/SphereManager.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class SphereManager : MonoBehaviour
 {
+    public int maximumSphereCount = 5;
+    public int currentSphereCount = 0;
+    public int spawnTimerSeconds = 10;
+
+    public GameObject sphereTracker;
     public GameObject spherePrefab;
-    // Start is called before the first frame update
+
+    private float spawnerCountdown = 0;
     void Start()
     {
-        Instantiate(spherePrefab, transform.position, Quaternion.identity);
-        Instantiate(spherePrefab, transform.position, Quaternion.identity);
-        Instantiate(spherePrefab, transform.position, Quaternion.identity);
-        Instantiate(spherePrefab, transform.position, Quaternion.identity);
-        Instantiate(spherePrefab, transform.position, Quaternion.identity);
-        Instantiate(spherePrefab, transform.position, Quaternion.identity);
-        Instantiate(spherePrefab, transform.position, Quaternion.identity);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        spawnerCountdown -= Time.deltaTime;
+        if (spawnerCountdown <= 0)
+        {
+            spawnerCountdown = spawnTimerSeconds;
+            if(sphereTracker.transform.childCount < maximumSphereCount)
+            {
+                Vector3 spawnPositionRandomness = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+                GameObject newSphere = Instantiate(spherePrefab, this.transform.position + spawnPositionRandomness, Quaternion.identity, sphereTracker.transform);
+            }
+        }
     }
 }
